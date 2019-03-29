d9ec5797678b58fa256a4a45923a626ccf173169
ahonn <ahonn95@outlook.com>
2019-03-29 22:39:05 +0800

    feat: add issueId, 1.0.1

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,9 @@
 module.exports = {
   wrapMarkdownTemplate(issue) {
-    const { title, body, created_at, labels } = issue;
+    const { title, body, created_at, labels, number } = issue;
 
     const tags = '[' + labels.map(({ name }) => `"${name}"`).join(',') + ']';
-    const markdown = `---\ntitle: "${title}"\ndate: "${created_at}"\ntags: ${tags}\n---\n\n${body}`;
+    const markdown = `---\ntitle: "${title}"\ndate: "${created_at}"\ntags: ${tags}\nissueId: ${number}\n---\n\n${body}`;
 
     return markdown;
   },
